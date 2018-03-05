01333bc14bd70952f4535a3dfbda2e902ec9df9b
czm_work <czmudk@163.com>
2018-03-05 15:49:34 +0800

    test shell

---
+++ Linux/shell/code/tianyun2/yuan_echo.sh
@@ -0,0 +1,17 @@
+#/bin/usr/env bash
+#* ? () [] {}
+echo *
+echo -e "t\tt"
+echo -e "t\nt"
+
+#30~37 is fg color
+echo -e "\e[1;30m"
+echo -e "\e[1;31m"
+echo -e "\e[1;33m"
+echo -e "\e[1;0m"
+
+#40~47 is bg color
+echo -e "\e[1;40m"
+echo -e "\e[1;41m"
+echo -e "\e[1;42m"
+echo -e "\e[1;0m"
